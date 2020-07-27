5fd6b9cce701e7fc29b3bef09e7d792017c2bfee
chahiinaz <chahiinaz@gmail.com>
2020-07-27 23:28:45 +0200

    Create Navigation component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+  <Navigation>
+ { /*<Logo>
+  <ImageLinkForm>
+  <FaceRecognition>*/}
+  </div>;
 }
 
 export default App;
