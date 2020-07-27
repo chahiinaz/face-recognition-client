2bfd957d4b9b54de2f79842d7b33cff73906653d
chahiinaz <chahiinaz@gmail.com>
2020-07-27 23:57:07 +0200

    Add Tilt package to create logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Tilt from "react-tilt";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
+        ❤️
+        <div className="Tilt-inner pa3"> </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
