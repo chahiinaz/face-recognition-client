d4a19e66850ec84f14bfac7337657d84205294d9
chahiinaz <chahiinaz@gmail.com>
2020-07-29 23:05:55 +0200

    Signup in front end and receive jwt token

--- src/App.js
+++ src/App.js
@@ -37,7 +37,12 @@ class App extends Component {
       isSignedIn: false,
     };
   }
-
+  componentDidMount() {
+    fetch("http://localhost:4000")
+      .then((response) => response.json())
+      .then(console.log);
+  }
+  
   calculateFaceLocation = (data) => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
