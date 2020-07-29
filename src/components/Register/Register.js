486881ffd080dbe6db84675e61b06daca4619a6c
chahiinaz <chahiinaz@gmail.com>
2020-07-29 20:55:40 +0200

    Change form tag to div tag in components for future json use

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,7 +5,7 @@ const Register = ({ onRouteChange }) => {
     <div>
       <article className="br4 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
-          <form className="measure ">
+          <div className="measure ">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f2 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
@@ -50,7 +50,7 @@ const Register = ({ onRouteChange }) => {
                 value="Register"
               />
             </div>
-          </form>
+          </div>
         </main>
       </article>
     </div>
