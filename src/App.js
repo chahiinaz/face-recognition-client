e5e0bc8f104da62ba63398d2357e11229e391a51
chahiinaz <chahiinaz@gmail.com>
2020-07-30 14:32:00 +0200

    Displaying name and entry count when signed in

--- src/App.js
+++ src/App.js
@@ -120,7 +120,10 @@ class App extends Component {
         {this.state.route === "home" ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
